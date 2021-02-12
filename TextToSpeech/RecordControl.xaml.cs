eebf01d3b8015694090e6a30bc3720563ef00b36
ftianf <790947404.com>
2021-02-12 11:32:04 +0800

    texttospeech

--- TextToSpeech/RecordControl.xaml.cs
+++ TextToSpeech/RecordControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,58 @@ namespace TextToSpeech
         public RecordControl()
         {
             InitializeComponent();
+
+
+            LoadButton.Click += BrowseButtonClickCallback;
+
         }
+
+
+        //有点意思，我需要写一个异步的读取QQBase64位加密的轮子
+        
+
+
+        private void BrowseButtonClickCallback(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Forms.OpenFileDialog data = new System.Windows.Forms.OpenFileDialog();
+
+            data.Filter = "(*.mht)|*.mht|All files (*.*)|*.*";
+
+            //   data.ShowDialog();
+            if (data.ShowDialog() == System.Windows.Forms.DialogResult.OK)//用于指示让文件夹展开对文件选中才允许运行。
+            {
+                var T = data.OpenFile();
+
+               // WebBrowser
+
+                web.NavigateToStream(data.OpenFile());
+
+                
+                //StreamReader streamReader = new StreamReader(data.OpenFile(), Encoding.UTF8);
+                //string tempData = streamReader.ReadToEnd();
+                //LogPath = TextBox.Text = data.FileName;//取得名称并输入到界面上。
+
+                TextBox.Text = data.FileName;
+
+                //#endregion
+
+                //TextDispose(tempData);
+            }
+
+
+
+            data.Dispose();
+
+            // 是否满足生成条件();
+        }
+
+        public void 读取(object sender, RoutedEventArgs e)
+        {
+
+
+            //web
+        }
+
     }
 }
--- TextToSpeech/TextTTool/TextTool.cs
+++ TextToSpeech/TextTTool/TextTool.cs
@@ -7,13 +7,13 @@ using System.Text.RegularExpressions;
 using System.IO;
 
 
-  static  class TextTool
-    {
-       
+static class TextTool
+{
+
 
 
     public static List<TextData> QQLogSplit(string text)
-        {
+    {
 
         string[] SingleConversation = Regex.Split(text, "\\r\\n\\r\\n", RegexOptions.IgnoreCase);//靠非-正则提取每句话部分
 
@@ -21,51 +21,18 @@ using System.IO;
         foreach (string Convesation in SingleConversation)
         {
             TextData data = new TextData();
-             data.SetTextData(Convesation);
+            data.SetTextData(Convesation);
             dataList.Add(data);
         }
         return dataList;
-            //MatchCollection
-
 
     }
 
     public static void LoadText(string Path)
     {
-        string str = File.ReadAllText(Path,Encoding.UTF8);
+        string str = File.ReadAllText(Path, Encoding.UTF8);
 
 
     }
-
-
-
-
-     
-
-
-
-
-    //string a = "2019 - 07 - 31 20:09:43 ftianf(790947404)伊斯科 / 约翰 你们迷糊的醒来，本能的坐了起来，发现自己身处于不明金属制成的舱内，舱内还微微冒着白色烟，你们感到寒冷，注意到全身裸体，因寒冷不想在这舱内多呆一步遍本能的爬了出来";
-
-    //MatchCollection MatchSpilit = Regex.Matches(Text, "[\u4e00-\u9fa5_a-zA-Z0-9]*\n");
-    // MatchCollection MatchSpilit = Regex.Matches(Text, "[\\w-:()/ ]*");
-    // MatchCollection MatchSpilit = Regex.Matches(Text, "((?!hello).)+$/");
-    // MatchCollection MatchSpilit = Regex.Matches(text, "[\\w-:()/，。 ]+");
-
-    // MatchCollection MatchSpilit = Regex.Matches(text, "[\\w-:()/，。 ]+(?=\\n\\r\\n)");
-    //MatchCollection MatchSpilit = Regex.Matches(text, "(?<!\\r\\n\\r\\n)[\\w-:()/，。 ]+");
-    //MatchCollection MatchSpilit = Regex.Matches(text, "(?<=\\r\\n\\r\\n)[\\w-:()/，。 ]+");
-
-
-    //    foreach (Match match in MatchSpilit)
-    //    {
-    //        Debug.Log(match.Value);
-    //    }
-
-
-    //    foreach (string g in G)
-    //    {
-    //        Debug.Log(g);
-    //    }
-    }
+}
 
